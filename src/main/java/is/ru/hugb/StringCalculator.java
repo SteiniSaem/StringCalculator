cf2d10c97a96618b76fd3a27f1c024b30f1a160e
SteiniSaem <thorsteinns16@ru.is>
2017-10-10 10:28:50 +0000

    Allows 0, 1 or 2 one digit numbers

--- src/main/java/is/ru/hugb/StringCalculator.java
+++ src/main/java/is/ru/hugb/StringCalculator.java
@@ -3,6 +3,28 @@ package is.ru.hugb.StringCalculator;
 
 public class StringCalculator
 {
+	public static int Add(String numbers)
+	{
+		if(numbers.length() == 0)
+		{
+			return 0;
+		}
+		
+		if(numbers.length() == 1)
+		{
+			char a = numbers.charAt(0);
+			int num = Character.getNumericValue(a);
+			return num;
+		}
+		if(numbers.length() == 2)
+		{
+			char a = numbers.charAt(0);
+			int num1 = Character.getNumericValue(a);
+			char b = numbers.charAt(2);
+			int num2 = Character.getNumericValue(b);
+			return num1 + num2;
+		}
+	}
 	
 	public static void main(String[] args) {
 
